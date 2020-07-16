90863bf56343551fe8a461617ab73f2f485e9699
Inchill <173772140@qq.com>
2020-07-16 15:02:12 +0800

    Update aliOss.ts
    delete access key and secret

--- client/config/aliOss.ts
+++ client/config/aliOss.ts
@@ -3,8 +3,8 @@ const OSS = require('ali-oss');
 const client = new OSS({
     bucket: 'chuck-blog-images',
     region: 'oss-cn-beijing',
-    accessKeyId: 'LTAI4GAuVfSvi8JNgkrTrFDC',
-    accessKeySecret: 'gn5xzH9BLJWll0iB9y01TqXAcjCk06'
+    accessKeyId: '',
+    accessKeySecret: ''
 });
 
 export default client;
